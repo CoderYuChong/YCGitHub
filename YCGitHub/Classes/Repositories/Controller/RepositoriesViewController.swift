612fa00f2c14a42eefcd69e0df0a5b97c3fbbf7c
yuchong <13088861503@163.com>
2018-02-01 21:33:10 +0800

    初始化

---
+++ YCGitHub/Classes/Repositories/Controller/RepositoriesViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RepositoriesViewController.swift
+//  YCGitHub
+//
+//  Created by 余冲 on 2018/2/1.
+//  Copyright © 2018年 YoukaStation. All rights reserved.
+//
+
+import UIKit
+
+class RepositoriesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
